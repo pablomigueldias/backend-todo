bc5fbeb6e56278db26002a8b44a08a0253687850
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-15 18:23:58 -0300

    test(users): Adiciona testes para o endpoint POST de usuário

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,20 @@ def test_root_deve_retornar_ola_mundo():
     assert response.json() == {'message': 'Hello, World!'}
     assert response.status_code == HTTPStatus.OK
 
+
+def test_user():
+    client = TestClient(app)
+    response = client.post(
+        '/users',
+        json={
+            'username': 'pablo',
+            'password': 'testpass',
+            'email': 'pablo@exemple.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'pablo',
+        'email': 'pablo@exemple.com',
+    }
