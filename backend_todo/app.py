e386fe60890a9f45c04386d7ceb7540bd611256e
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-15 17:00:58 -0300

    chore: Configuração inicial do Uvicorn e startup da aplicação

--- backend_todo/app.py
+++ backend_todo/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from backend_todo.schemas import Message
+
+app = FastAPI(title='Todo Backend API')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello, World!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,4 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {'message': 'Hello, World!'}
     assert response.status_code == HTTPStatus.OK
+
