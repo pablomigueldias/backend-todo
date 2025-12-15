b0e9d584ceec38dcd44257736ccb7fc0c83d5c73
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-15 18:25:28 -0300

    feat(schemas): Cria modelos Pydantic para User

--- backend_todo/schemas.py
+++ backend_todo/schemas.py
@@ -1,5 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Message(BaseModel):
     message: str
+
+
+class User(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+
+
+class UsersPublic(BaseModel):
+    username: str
+    email: EmailStr
+    id: int
+
+
+class UserDB(User):
+    id: int
