41a613037fa70ff6bdb847440bc08f76ce42e1ea
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-16 11:57:59 -0300

    test: Adiciona testes de integração para endpoints GET, PUT, e DELETE

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from backend_todo.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
-    response = client.get('/')
-
-    assert response.json() == {'message': 'Hello, World!'}
-    assert response.status_code == HTTPStatus.OK
-
-
-def test_user():
-    client = TestClient(app)
+def test_user(client):
     response = client.post(
         '/users',
         json={
@@ -29,3 +16,61 @@ def test_user():
         'username': 'pablo',
         'email': 'pablo@exemple.com',
     }
+
+
+def test_list_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'id': 1, 'username': 'pablo', 'email': 'pablo@exemple.com'}]
+    }
+
+
+def test_update_users(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'pablo',
+            'password': 'testpass',
+            'email': 'pablo@exemple.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'pablo',
+        'email': 'pablo@exemple.com',
+    }
+
+
+def test_update_users_not_found(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'pablo',
+            'password': 'testpass',
+            'email': 'pablo@exemple.com',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    expected_response = {
+        'id': 1,
+        'username': 'pablo',
+        'email': 'pablo@exemple.com',
+        'password': 'testpass',
+    }
+
+    assert response.json() == expected_response
+    assert response.status_code == HTTPStatus.OK
+
+
+def teste_delete_user_not_found(client):
+    response = client.delete('/users/0')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json().get('detail') == 'Não encontrado'
