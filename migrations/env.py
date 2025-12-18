b916ba8d846f3ec27ccb3b5fc57218162dc51627
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-17 23:24:26 -0300

    chore(config): adicionando configurações

--- migrations/env.py
+++ migrations/env.py
@@ -10,7 +10,7 @@ from backend_todo.settings import Settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL) #type: ignore
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
