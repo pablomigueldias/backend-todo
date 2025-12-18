3072b6e9b09362c852c644c2adbac6894185047b
Pablo Ortiz <pablo.miguel.dias@gmail.com>
2025-12-18 19:56:25 -0300

    test: Refatora conftest e testes para suportar banco de dados SQLite

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,32 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 from backend_todo.app import app
-from backend_todo.models import table_registry
+from backend_todo.database import get_session
+from backend_todo.models import User, table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
 
     table_registry.metadata.create_all(engine)
 
@@ -44,3 +57,13 @@ def _mock_db_time(model, time=datetime(2025, 12, 18)):
 @pytest.fixture
 def mock_db_time():
     return _mock_db_time
+
+
+@pytest.fixture
+def user(session: Session):
+    user = User(username='Pablo', email='pablo@pablo.com', password='segredo')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from backend_todo.schemas import UsersPublic
+
 
 def test_user(client):
     response = client.post(
@@ -21,12 +23,17 @@ def test_user(client):
 def test_list_users(client):
     response = client.get('/users')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [{'id': 1, 'username': 'pablo', 'email': 'pablo@exemple.com'}]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_users(client):
+def test_list_user_with_users(client, user):
+    user_schema = UsersPublic.model_validate(user).model_dump()
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_users(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -55,15 +62,10 @@ def test_update_users_not_found(client):
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
-    expected_response = {
-        'id': 1,
-        'username': 'pablo',
-        'email': 'pablo@exemple.com',
-        'password': 'testpass',
-    }
+    expected_response = {'message': 'User deleted'}
 
     assert response.json() == expected_response
     assert response.status_code == HTTPStatus.OK
@@ -73,4 +75,26 @@ def teste_delete_user_not_found(client):
     response = client.delete('/users/0')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json().get('detail') == 'Não encontrado'
+    assert response.json().get('detail') == 'User not found'
+
+
+def test_update_integrity_erro(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@exemple.com',
+            'password': 'secret',
+        },
+    )
+    response = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CONFLICT
+    assert response.json() == {'detail': 'Username or Email already exists'}
